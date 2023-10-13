337ba6cfecf74759173534ace0ea6902603e4c45
Muguntha96 <muguntharamkumar1996@gmail.com>
2023-10-13 19:02:27 -0300

    create commentSchema

--- models/post.js
+++ models/post.js
@@ -1,15 +1,22 @@
 import mongoose from "mongoose"
 
 const Schema=mongoose.Schema
+const commentSchema=new Schema({
+  text:{type:String,required:true},
+  author:{type:Schema.Types.ObjectId,ref:'Profile'}
+},{
+  timestamps:true
+})
+
 const postSchema=new Schema({
   title:{type:String,required:true},
   location:{type:String,required:true},
   content:{type:String,required:true},
   author:{type:Schema.Types.ObjectId,ref:'Profile'},
   recommendations:[],
-  comments:[],
+  comments:[commentSchema],
   public:Boolean,
-  likes:Boolean
+  likes:{type:Schema.Types.ObjectId,ref:'Profile'}
 },{
   timestamps:true
 })
