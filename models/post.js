315ef1c11a18a527a46a3a3feec333868fa5b42f
Muguntha96 <muguntharamkumar1996@gmail.com>
2023-10-13 19:46:45 -0300

    create recommendation model

--- models/post.js
+++ models/post.js
@@ -7,13 +7,33 @@ const commentSchema=new Schema({
 },{
   timestamps:true
 })
+const recommendationSchema=new Schema({
+  activityName:{
+    type:String,
+    required:true,
+    enum:['Hiking','Star Gazing','Cycling & Mountain Biking','Whitewater Rafting','Camping'],
+  },
+  timing:{
+    from:{type:time,required:true},
+    to:{type:time,required:true}
+  },
+  rating:{
+    type:Number,
+    min:1,
+    max:5,
+    default:5,
+    required:true
+  }
+},{
+  timestamps:true
+})
 
 const postSchema=new Schema({
   title:{type:String,required:true},
   location:{type:String,required:true},
   content:{type:String,required:true},
   author:{type:Schema.Types.ObjectId,ref:'Profile'},
-  recommendations:[],
+  recommendations:[recommendationSchema],
   comments:[commentSchema],
   public:Boolean,
   likes:{type:Schema.Types.ObjectId,ref:'Profile'}
